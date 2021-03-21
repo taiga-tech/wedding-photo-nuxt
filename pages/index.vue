952617611f89e224c3bfbd0e5b37146bbea2e6ab
taiga-tech <taiga.dev.n@gmail.com>
2021-03-21 22:44:28 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center"></div>
+      <v-card>
+        <pre v-if="posts">{{ posts }}</pre>
+        <v-btn @click="getApi">go</v-btn>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: null,
+    }
+  },
+  methods: {
+    async getApi() {
+      const response = await this.$axios.$get('/api/posts/')
+
+      this.posts = response
+    },
+  },
+}
+</script>
