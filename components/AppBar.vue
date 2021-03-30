6e1f0981cface87f5a06390c0824140d3863d6a8
taiga-tech <taiga.dev.n@gmail.com>
2021-03-31 03:29:05 +0900

    Update default.vue

---
+++ components/AppBar.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer" clipped floating app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-spacer />
+
+      <v-list>
+        <v-list-item v-if="check" @click="logout">
+          <v-list-item-action>
+            <v-icon>mdi-account-arrow-right-outline</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>ログアウト</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar clipped-left fixed app dense flat>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
+      <v-toolbar-title v-text="user ? user.name : title" />
+
+      <v-spacer />
+
+      <v-btn v-if="!check" to="/login">ログイン</v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'ホーム',
+          to: '/',
+        },
+        {
+          icon: 'mdi-phone',
+          title: 'お問い合わせ',
+          to: '/info/contact/',
+        },
+        {
+          icon: 'mdi-github',
+          title: '作成者について',
+          to: '/info/',
+        },
+      ],
+      title: 'weddingphoto',
+    }
+  },
+
+  computed: {
+    user() {
+      return this.$store.getters['auth/user']
+    },
+    check() {
+      return this.$store.getters['auth/check']
+    },
+  },
+
+  methods: {
+    async logout() {
+      await this.$store.dispatch('auth/logout').then(() => {
+        this.$router.push('/')
+      })
+    },
+  },
+}
+</script>
