52a1d19908e47f3ed64e61cd881adcbc41e356b5
taiga-tech <taiga.dev.n@gmail.com>
2021-04-05 23:35:33 +0900

    Create AppAlert.vue

---
+++ components/AppAlert.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-alert
+    :value="error ? true : false"
+    type="error"
+    outlined
+    transition="slide-y-transition"
+    dismissible
+    >{{ error }}
+  </v-alert>
+</template>
+
+<script>
+export default {
+  props: {
+    error: { type: Object, default: null },
+  },
+}
+</script>
