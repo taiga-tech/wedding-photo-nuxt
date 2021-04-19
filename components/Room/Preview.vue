e83a5d4a3cc34a393c8e13a2cfeef3bca87d154f
taiga-tech <taiga.dev.n@gmail.com>
2021-04-19 23:48:17 +0900

    Update Preview.vue

--- components/Room/Preview.vue
+++ components/Room/Preview.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="d-flex my-5" style="width: 100%; flex-wrap: wrap">
-    <div v-for="(img, i) in previews" :key="i" style="width: 33%">
-      <v-card>
-        <v-img :src="img.path" height="100">
-          <template>
-            <v-btn icon x-small color="red" @click="removePreviews(i)">
-              <v-icon>mdi-close-circle</v-icon>
-            </v-btn>
-          </template>
-        </v-img>
-      </v-card>
+  <div class="pb-5">
+    <div class="d-flex" style="width: 100%; flex-wrap: wrap">
+      <div v-for="(img, i) in previews" :key="i" style="width: 33%">
+        <v-card>
+          <v-img :src="img.path" height="100">
+            <template>
+              <v-btn icon x-small color="pink" @click="removePreviews(i)">
+                <v-icon>mdi-close-circle</v-icon>
+              </v-btn>
+            </template>
+          </v-img>
+        </v-card>
+      </div>
     </div>
   </div>
 </template>
