c24c7466148f4238a8842a45ca2b00d4031528b5
taiga-tech <taiga.dev.n@gmail.com>
2021-04-21 06:49:59 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,41 @@
 <template>
   <v-container>
-    <div>
-      <div>
+    <div align="center">
+      <h1 class="my-10 text-h3 headTitle">
+        Wedding Photo Album<br />
+        みんなでひとつだけのフォトアルバム
+      </h1>
+
+      <div class="my-12">
         <auth-login v-if="!check"></auth-login>
         <div v-if="check">
-          <v-btn :to="`/room/${user.id}/`">
-            <v-icon left>mdi-table-large</v-icon>
-            {{ user.name }}に入室する</v-btn
-          >
+          <v-btn :to="`/room/${user.id}/`" color="pink" large>
+            <v-icon left>mdi-play</v-icon>
+            {{ user.name }}のアルバム
+          </v-btn>
         </div>
       </div>
+
+      <p class="mx-auto mb-10" style="max-width: 568px">
+        <b>
+          大切なイベントの思い出を<br />
+          みんなでひとつだけのフォトアルバムに作っていくアプリです。<br />
+          たくさんの思い出を詰め込みましょう！！
+        </b>
+      </p>
+
+      <div class="d-flex">
+        <v-img src="/img/top_demo_desktop.png" width="75%" contain></v-img>
+        <v-img
+          src="/img/top_demo_mobile.jpg"
+          width="25%"
+          aspect-ratio="0.48976926"
+          contain
+        >
+        </v-img>
+      </div>
     </div>
+
     <app-footer />
   </v-container>
 </template>
@@ -20,9 +45,26 @@ import AuthComputed from '~/assets/mixins/AuthComputed.js'
 
 export default {
   mixins: [AuthComputed],
-  // https://insta-window.studio.site/
+
   head() {
     return { title: null }
   },
 }
 </script>
+
+<style scoped>
+.headTitle {
+  display: block;
+  background-image: -webkit-linear-gradient(
+    270.97deg,
+    rgb(255, 229, 128) -21.36%,
+    rgb(255, 117, 113) -2.45%,
+    rgb(234, 93, 173) 26.84%,
+    rgb(194, 160, 253) 64.15%,
+    rgb(59, 240, 228) 108.29%,
+    rgb(178, 244, 182) 159.03%
+  );
+  -webkit-text-fill-color: rgba(0, 0, 0, 0);
+  -webkit-background-clip: text;
+}
+</style>
