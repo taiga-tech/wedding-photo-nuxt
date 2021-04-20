c9ad3dbd4238d5bb5895547b351b9aca50785b99
taiga-tech <taiga.dev.n@gmail.com>
2021-04-21 06:49:37 +0900

    Update Login.vue

--- components/Auth/Login.vue
+++ components/Auth/Login.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <v-btn v-if="!check" @click="openLogin = !openLogin">
-      ログインして写真を見る
+    <v-btn color="pink" large class="my-2" @click="openLogin = !openLogin">
+      <v-icon left>mdi-play</v-icon> ログインして写真を見る
+    </v-btn>
+    <v-btn color="pink" outlined x-large to="/demo/" class="my-2">
+      デモを体験する
     </v-btn>
 
     <v-dialog v-model="openLogin" width="500">
