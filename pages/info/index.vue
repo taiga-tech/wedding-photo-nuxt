c68387fac04b07425fdf8387701a0e3ea2f6a48f
taiga-tech <taiga.dev.n@gmail.com>
2021-04-22 02:00:53 +0900

    Update index.vue

--- pages/info/index.vue
+++ pages/info/index.vue
@@ -12,7 +12,36 @@
           </v-avatar>
           仲野 大雅
         </h1>
-        <p class="px-10">"全力で楽をする"ために毎日開発しています。</p>
+        <p>"全力で楽をする"ために毎日開発しています。</p>
+        <h2 class="mb-2">このアプリについて</h2>
+        <p>
+          このアプリは友人の結婚式のために作成しました。<br />
+          コードは以下の3つのリポジトリに公開されています。
+        </p>
+        <v-list style="max-width: 400px">
+          <template v-for="(product, i) in products">
+            <v-list-item
+              :key="i"
+              :href="product.href"
+              target="_blank"
+              rel="noopener"
+            >
+              <v-list-item-avatar>
+                <v-icon large :color="product.color"
+                  >mdi-{{ product.icon }}</v-icon
+                >
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <b>{{ product.name }}</b>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  {{ product.about }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list>
       </div>
 
       <v-card-text class="px-12">
@@ -43,6 +72,30 @@ export default {
   mixins: [UaFilters],
   data() {
     return {
+      products: [
+        {
+          name: 'wedding-photo-api',
+          icon: 'laravel',
+          color: '#ff291a',
+          href: 'https://github.com/taiga-tech/wedding-photo-api/',
+          about: 'バックエンド at AWS ECS Fargate',
+        },
+        {
+          name: 'wedding-photo-nuxt',
+          icon: 'nuxt',
+          color: '#04c58e',
+          href: 'https://github.com/taiga-tech/wedding-photo-nuxt/',
+          about: 'フロントエンド at Netlify',
+        },
+        {
+          name: 'lambda-edge-image-convert',
+          icon: 'nodejs',
+          color: '#67a063',
+          href: 'https://github.com/taiga-tech/lambda-edge-image-convert/',
+          about: '画像最適化処理 at AWS Lambda',
+        },
+      ],
+
       snsList: [
         {
           name: 'ホームページ',
