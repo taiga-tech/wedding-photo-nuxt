fa42f13bc6a03e2b8028753251d70efffa6aa155
taiga-tech <taiga.dev.n@gmail.com>
2021-04-24 03:59:33 +0900

    Update index.vue

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,27 +1,18 @@
 <template>
-  <v-container style="height: 100%" class="d-flex align-center">
-    <div class="mx-auto" style="max-width: 940px">
-      <div class="mx-auto" style="max-width: 600px">
-        <app-alert :error="error" />
+  <base-container :card="false" max-width="600">
+    <app-alert :error="error" />
 
-        <auth-form />
+    <auth-form />
 
-        <v-dialog v-model="loading" persistent width="300">
-          <v-card color="deep-purple">
-            <v-card-text>
-              現在ログインしています
-              <v-progress-linear
-                indeterminate
-                color="white"
-                class="mb-0"
-              ></v-progress-linear>
-            </v-card-text>
-          </v-card>
-        </v-dialog>
-      </div>
-      <app-footer />
-    </div>
-  </v-container>
+    <v-dialog v-model="loading" persistent width="300">
+      <v-card color="deep-purple">
+        <v-card-text>
+          QRコードを読み込みました。現在ログインしています
+          <v-progress-linear indeterminate color="white" class="mb-0" />
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </base-container>
 </template>
 
 <script>
