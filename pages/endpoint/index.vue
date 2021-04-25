724f7af1510c934e8f559a74918e3e71adfbe281
taiga-tech <taiga.dev.n@gmail.com>
2021-04-26 04:52:04 +0900

    Update index.vue

--- pages/endpoint/index.vue
+++ pages/endpoint/index.vue
@@ -1,24 +1,17 @@
 <template>
-  <h1>{{ data }}</h1>
+  <base-container>
+    <pre>{{ response }}</pre>
+  </base-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      data: null,
-    }
-  },
-
-  mounted() {
-    this.$axios
-      .get('/api/hello')
-      .then((response) => {
-        this.data = response.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+  async asyncData({ $axios }) {
+    const response = await $axios.get('/api/hello').catch((error) => {
+      console.log(error)
+      return { error }
+    })
+    return { response }
   },
 }
 </script>
