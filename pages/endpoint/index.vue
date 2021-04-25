e6a0a48f4464147a82fc26c7d52cc5514f45b3ff
taiga-tech <taiga.dev.n@gmail.com>
2021-04-26 03:45:29 +0900

    Create index.vue

---
+++ pages/endpoint/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ data }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: null,
+    }
+  },
+
+  mounted() {
+    this.$axios
+      .get('/api/hello')
+      .then((response) => {
+        this.data = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  },
+}
+</script>
