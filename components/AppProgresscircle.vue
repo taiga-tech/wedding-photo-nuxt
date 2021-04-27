68d67cd52d4c34cc1c609c575360b8958f8c6ac7
taiga-tech <taiga.dev.n@gmail.com>
2021-04-27 20:46:49 +0900

    Create AppProgresscircle.vue

---
+++ components/AppProgresscircle.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-row class="fill-height ma-0" align="center" justify="center">
+    <v-progress-circular
+      aria-label="loading"
+      indeterminate
+      color="green lighten-5"
+    ></v-progress-circular>
+  </v-row>
+</template>
