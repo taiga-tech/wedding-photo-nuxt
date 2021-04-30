9612a01e34f277d16214357a4760c60a407bc62c
taiga-tech <taiga.dev.n@gmail.com>
2021-05-01 06:11:16 +0900

    Update index.vue

--- pages/room/_roomId/index.vue
+++ pages/room/_roomId/index.vue
@@ -2,8 +2,6 @@
   <v-container fluid>
     <app-alert :error="error" />
 
-    <room-new ref="roomNew" />
-
     <v-dialog
       v-if="modalsrc"
       v-model="openmodal"
@@ -12,7 +10,7 @@
     >
       <v-card height="100%" flat tile>
         <v-app-bar dense>
-          <v-btn icon color="pink" @click="openmodal = false">
+          <v-btn icon color="pink" @click="closemodal">
             <v-icon>mdi-close</v-icon>
           </v-btn>
           <v-app-bar-title>@{{ modalsrc.nickname }}</v-app-bar-title>
@@ -20,7 +18,22 @@
           <v-spacer />
 
           <v-toolbar-items>
-            <v-btn text color="primary"> Save </v-btn>
+            <v-menu offset-y>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn color="primary" text dark v-bind="attrs" v-on="on">
+                  save
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item
+                  v-for="(item, index) in items"
+                  :key="index"
+                  @click="download(index, pullIndex, modalsrc)"
+                >
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
           </v-toolbar-items>
         </v-app-bar>
 
@@ -40,7 +53,6 @@
               right: () => swipe(-1),
             }"
             :src="conversion(awsCdnUrl, modalsrc.photos[pullIndex].path)"
-            :lazy-src="conversion(awsCdnUrl, 'img/lazy.jpg')"
             :alt="modalsrc.photos[pullIndex].path"
             :aspect-ratio="modalsrc.photos[pullIndex].aspect"
             contain
@@ -50,12 +62,16 @@
             <v-card-text v-if="modalsrc.message">
               {{ modalsrc.message }}
             </v-card-text>
+            <template v-slot:placeholder>
+              <app-progresscircle />
+            </template>
           </v-img>
         </v-card>
+
         <v-card v-if="modalsrc.photos.length >= 2" flat tile>
           <client-only>
             <div
-              v-masonry="containerId"
+              v-masonry="'preContainerId'"
               transition-duration="0.1s"
               item-selector=".item"
               class="masonryWrap"
@@ -63,7 +79,7 @@
               <div
                 v-for="(preview, i) in modalsrc.photos"
                 :key="preview.path"
-                v-masonry-tile="containerId"
+                v-masonry-tile="'preContainerId'"
                 class="item"
               >
                 <v-card
@@ -95,28 +111,30 @@
         item-selector=".item"
         class="masonryWrap"
       >
-        <div
-          v-for="preview in previews"
-          :key="preview.path"
-          v-masonry-tile="containerId"
-          class="item"
-        >
-          <v-card hover :loading="loading" :disabled="loading">
-            <v-img
-              :src="conversion(awsCdnUrl, preview.path)"
-              :lazy-src="conversion(awsCdnUrl, 'img/lazy.jpg')"
-              :aspect-ratio="preview.aspect"
-              :alt="preview.path"
-              :width="width"
-              class="fill-height"
-            ></v-img>
-          </v-card>
-        </div>
+        <template v-if="previews.length !== 0">
+          <div
+            v-for="preview in previews"
+            :key="preview.path"
+            v-masonry-tile="containerId"
+            class="item"
+          >
+            <v-card :loading="loading" :disabled="loading">
+              <v-img
+                :src="conversion(awsCdnUrl, preview.path)"
+                :lazy-src="conversion(awsCdnUrl, 'img/lazy.jpg')"
+                :aspect-ratio="preview.aspect"
+                :alt="preview.path"
+                :width="width"
+                class="fill-height"
+              ></v-img>
+            </v-card>
+          </div>
+        </template>
 
         <div v-for="(post, i) in posts" :key="i">
           <v-card
             v-for="(photo, index) in post.photos"
-            :key="index"
+            :key="photo.path"
             v-masonry-tile="containerId"
             hover
             class="item"
@@ -133,6 +151,13 @@
               @load="refresh"
             >
               <v-card-text>@{{ post.nickname }}</v-card-text>
+              <v-chip
+                x-small
+                outlined
+                style="position: absolute; bottom: 0; right: 0"
+              >
+                {{ $moment(post.created_at).fromNow() }}
+              </v-chip>
               <template v-slot:placeholder>
                 <app-progresscircle />
               </template>
@@ -141,16 +166,18 @@
         </div>
       </div>
     </client-only>
+    <room-new ref="roomNew" />
   </v-container>
 </template>
-
 <script>
 import AuthComputed from '~/assets/mixins/AuthComputed.js'
 import UaFilters from '~/assets/mixins/UaFilters'
 import ImageSwipe from '~/assets/mixins/ImageSwipe'
+import Download from '~/assets/mixins/Download'
+import Meta from '~/assets/mixins/Meta'
 
 export default {
-  mixins: [AuthComputed, UaFilters, ImageSwipe],
+  mixins: [AuthComputed, UaFilters, ImageSwipe, Download, Meta],
 
   middleware: ['not_logined_user', 'is_admin', 'logined_user'],
 
@@ -169,6 +196,10 @@ export default {
       openmodal: false,
       modalsrc: null,
       pullIndex: null,
+      items: [{ title: 'この画像のみ保存' }, { title: '全て保存' }],
+      meta: {
+        title: null,
+      },
     }
   },
 
@@ -184,9 +215,19 @@ export default {
     },
   },
 
+  mounted() {
+    this.meta.title = this.user.name
+  },
+
   methods: {
     refresh() {
       this.$redrawVueMasonry(this.containerId)
+    },
+
+    closemodal() {
+      this.openmodal = false
+      this.pullIndex = null
+      this.modalsrc = null
     },
 
     getPhoto(post, index) {
