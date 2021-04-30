cd6bea08ee2c206fdff4f3dd547e57b89bd8f6aa
taiga-tech <taiga.dev.n@gmail.com>
2021-05-01 06:07:38 +0900

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,3 +7,31 @@
     </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  computed: {
+    currentPageForCanonical() {
+      return this.$route.path.slice(1)
+    },
+  },
+
+  head() {
+    return {
+      meta: [
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: `${process.env.APP_URL}${this.currentPageForCanonical}`,
+        },
+      ],
+      link: [
+        {
+          rel: 'canonical',
+          href: `${process.env.APP_URL}${this.currentPageForCanonical}`,
+        },
+      ],
+    }
+  },
+}
+</script>
